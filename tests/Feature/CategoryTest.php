4d79b9c15219e13ca061445d544194f43795fa60
Miguel António <miguelabfonseca@gmail.com>
2022-11-16 12:50:41 +0000

    [UPD] testing

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -14,62 +14,54 @@ class CategoryTest extends TestCase
      */
     public function test_the_api_category_returns_a_successful_response()
     {
-        $response = $this->get('/api/categorias');
+        $response = $this->get('/categories');
         $response->assertStatus(200);
     }
 
     public function test_the_api_category_returns_a_json_response()
     {
-        $response = $this->get('/api/categorias');
+        $response = $this->get('/categories');
         $output = json_decode($response->getContent(), true);
         $this->assertArrayHasKey(1, $output);
     }
 
     public function test_the_api_category_returns_a_valid_json_structure_and_data()
     {
-        $response = $this->get('/api/categorias');
+        $response = $this->get('/categories');
         $this->assertJsonStringEqualsJsonFile(
             base_path('tests/Feature/json/categorias-schema.json'), $response->getContent());
     }
 
     public function test_the_api_category_returns_a_successful_response_for_one_id()
     {
-        $response = $this->get('/api/categorias/1');
+        $response = $this->get('/categories/1');
         $response->assertStatus(200);
     }
 
     public function test_the_api_category_returns_a_json_response_for_one_id()
     {
-        $response = $this->get('/api/categorias/1');
+        $response = $this->get('/categories/1');
         $output = json_decode($response->getContent(), true);
-        $this->assertArrayHasKey('cor', $output);
+        $this->assertArrayHasKey('color', $output);
     }
 
     public function test_the_api_category_returns_a_valid_json_structure_for_one_existing_category()
     {
-        $test = json_encode([
-            "id" => 2,
-            "titulo" => "Laravel",
-            "cor" => "Green",
-            "created_at" => "2022-11-01T13:50:56.000000Z",
-            "updated_at" => "2022-11-01T13:50:56.000000Z"
-        ]);
-
-        $response = $this->get('/api/categorias/2');
-        $this->assertJsonStringEqualsJsonString(
-            $test, $response->getContent()
+        $response = $this->get('/categories/2');
+        $this->assertJsonStringEqualsJsonFile(
+            base_path('tests/Feature/json/categoria-2-schema.json'), $response->getContent()
         );
     }
 
     public function test_the_api_video_returns_a_notfound_response_for_one_invalid_id()
     {
-        $response = $this->get('/api/categorias/99');
+        $response = $this->get('/categories/99');
         $response->assertStatus(404);
     }
 
     public function test_the_api_video_returns_a_valid_json_structure_for_one_invalid_id()
     {
-        $response = $this->get('/api/categorias/99');
+        $response = $this->get('/categories/99');
         $this->assertJsonStringEqualsJsonString(
             json_encode(['status' => 'error', 'message' => 'Category not found!']), $response->getContent()
         );
@@ -77,27 +69,27 @@ class CategoryTest extends TestCase
 
     public function test_the_api_video_returns_a_found_response_for_one_valid_category_id_with_videos()
     {
-        $response = $this->get('/api/categorias/2/videos');
+        $response = $this->get('/categories/2/videos');
         $this->assertJsonStringEqualsJsonFile(
             base_path('tests/Feature/json/categoria-videos-schema.json'), $response->getContent());
     }
 
     public function test_the_api_video_returns_a_found_response_status_for_one_valid_category_id_without_videos()
     {
-        $response = $this->get('/api/categorias/3');
+        $response = $this->get('/categories/3');
         $response->assertStatus(200);
     }
 
-    public function test_the_api_video_returns_a_found_response_for_one_valid_category_id_without_videos()
+    public function test_the_api_video_returns_a_videos_not_found_response_for_one_valid_category_id_without_videos()
     {
-        $response = $this->get('/api/categorias/3/videos');
+        $response = $this->get('/categories/99/videos');
         $this->assertJsonStringEqualsJsonString(
             json_encode(["status" => "error", "message" => "No videos found on the selected category!"]), $response->getContent());
     }
 
-    public function test_the_api_video_returns_a_notfound_response_status_for_one_valid_category_id_without_videos()
+    public function test_the_api_video_returns_a_not_found_response_status_for_one_valid_category_id_without_videos()
     {
-        $response = $this->get('/api/categorias/99');
+        $response = $this->get('/categories/99');
         $response->assertStatus(404);
     }
 
--- tests/Feature/VideoTest.php
+++ tests/Feature/VideoTest.php
@@ -14,64 +14,54 @@ class VideoTest extends TestCase
      */
     public function test_the_api_video_returns_a_successful_response()
     {
-        $response = $this->get('/api/videos');
+        $response = $this->get('/videos');
         $response->assertStatus(200);
     }
 
     public function test_the_api_video_returns_a_json_response()
     {
-        $response = $this->get('/api/videos');
+        $response = $this->get('/videos');
         $output = json_decode($response->getContent(), true);
         $this->assertArrayHasKey(1, $output);
     }
 
     public function test_the_api_video_returns_a_valid_json_structure_and_data()
     {
-        $response = $this->get('/api/videos');
+        $response = $this->get('/videos');
         $this->assertJsonStringEqualsJsonFile(
             base_path('tests/Feature/json/videos-schema.json'), $response->getContent());
     }
 
     public function test_the_api_video_returns_a_successful_response_for_one_id()
     {
-        $response = $this->get('/api/videos/1');
+        $response = $this->get('/videos/1');
         $response->assertStatus(200);
     }
 
     public function test_the_api_video_returns_a_json_response_for_one_id()
     {
-        $response = $this->get('/api/videos/1');
+        $response = $this->get('/videos/1');
         $output = json_decode($response->getContent(), true);
-        $this->assertArrayHasKey('titulo', $output);
+        $this->assertArrayHasKey('title', $output);
     }
 
     public function test_the_api_video_returns_a_valid_json_structure_for_one_existing_video()
     {
-        $test = json_encode([
-            "id" => 1,
-            'categoriaId' => 1,
-            "titulo" => "Criar API Rest",
-            "descricao" => "Como criar e testar e consumir uma api rest.",
-            "url" => "www.youtube.com/21312",
-            'created_at' => "2022-11-01T10:36:48.000000Z",
-            "updated_at" => "2022-11-01T10:36:48.000000Z",
-        ]);
-
-        $response = $this->get('/api/videos/1');
-        $this->assertJsonStringEqualsJsonString(
-            $test, $response->getContent()
+        $response = $this->get('/videos/1');
+        $this->assertJsonStringEqualsJsonFile(
+            base_path('tests/Feature/json/videos-1-schema.json'), $response->getContent()
         );
     }
 
     public function test_the_api_video_returns_a_notfound_response_for_one_invalid_id()
     {
-        $response = $this->get('/api/videos/99');
+        $response = $this->get('/videos/99');
         $response->assertStatus(404);
     }
 
     public function test_the_api_video_returns_a_valid_json_structure_for_one_invalid_id()
     {
-        $response = $this->get('/api/videos/99');
+        $response = $this->get('/videos/99');
         $this->assertJsonStringEqualsJsonString(
             json_encode(['status' => 'error', 'message' => 'Video not found!']), $response->getContent()
         );
@@ -79,14 +69,14 @@ class VideoTest extends TestCase
 
     public function test_the_api_video_returns_a_json_response_for_search()
     {
-        $response = $this->get('/api/videos?search=aplicaçã');
+        $response = $this->get('/videos?search=ap');
         $this->assertJsonStringEqualsJsonFile(
             base_path('tests/Feature/json/search-video-schema.json'), $response->getContent());
     }
 
     public function test_the_api_video_returns_a_json_response_for_search_not_found()
     {
-        $response = $this->get('/api/videos?search=2334445555');
+        $response = $this->get('/videos?search=2334445555');
         $response->assertStatus(404);
     }
 
